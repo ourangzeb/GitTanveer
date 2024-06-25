f09212113a0fb8ea60d2cd31149493ae917872aa
Ourang-Zeb Khan <khanourangzaib@gmail.com>
2024-06-25 14:13:39 +0300

    Qauto sample

---
+++ Qauto/src/main/java/com/thegroup/group/Qauto/QautoApplication.java
@@ -0,0 +1,13 @@
+package com.thegroup.group.Qauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QautoApplication.class, args);
+	}
+
+}
---
+++ Qauto/src/test/java/com/thegroup/group/Qauto/QautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegroup.group.Qauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
